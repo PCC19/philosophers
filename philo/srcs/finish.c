46af1dc4c62e6384a18562518b6a8d5bbb21b929
Paulo Cunha <pcunha00@hotmail.com>
2021-08-02 20:36:28 +0200

    refactored finish

--- philo/srcs/finish.c
+++ philo/srcs/finish.c
@@ -6,7 +6,7 @@
 /*   By: pcunha <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 19:02:54 by pcunha            #+#    #+#             */
-/*   Updated: 2021/08/02 19:09:35 by pcunha           ###   ########.fr       */
+/*   Updated: 2021/08/02 20:36:00 by pcunha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 void	finish(t_philo *philo, int *forks)
 {
-	free(philo);
-	free(forks);
+	if (philo != NULL)
+	{
+		free(philo);
+		philo = NULL;
+	}
+	if (forks != NULL)
+	{
+		free(forks);
+		forks = NULL;
+	}
 	exit(0);
 }
