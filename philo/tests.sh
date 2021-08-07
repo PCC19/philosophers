1259820c42ac1084ae97269f2fcac5c94e4aa578
Paulo Cunha <pcunha00@hotmail.com>
2021-08-07 14:29:39 -0300

    final

--- philo/tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-$(alias test1='./philo 1 800 200 200')
-$(alias test2='./philo 5 800 200 200')
-$(alias test3='./philo 5 800 200 200 7')
-$(alias test4='./philo 4 410 200 200')
-$(alias test5='./philo 4 310 200 100')
-
